9bad4178624b8f10f0580dcfe36e1e56b738ea39
MvvvD <68166614+MvvvD@users.noreply.github.com>
2023-07-18 07:38:14 +0200

    add script for creating required sql tables

---
+++ src/main/sql/create-required-tables.sql
@@ -0,0 +1,36 @@
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities4_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities4_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `employees` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(40) DEFAULT NULL,
+  `last_name` varchar(40) DEFAULT NULL,
+  `role` varchar(40) DEFAULT NULL,
+  `hired_since` date DEFAULT (curdate()),
+  `phone_number` varchar(40) DEFAULT NULL,
+  `current_task` varchar(800) DEFAULT NULL,
+  `task_timestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `tasks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(800) DEFAULT NULL,
+  `employee_id` int DEFAULT NULL,
+  `start_timestamp` datetime DEFAULT NULL,
+  `finish_timestamp` datetime NOT NULL DEFAULT (now()),
+  PRIMARY KEY (`id`),
+  KEY `employee_id` (`employee_id`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` char(68) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
